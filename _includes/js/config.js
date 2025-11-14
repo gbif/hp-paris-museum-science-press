4b7fa55f2bd459281cb06206efc6388bb20795cc
Chris (EJT) <chris.le-coquet-le-roux@mnhn.fr>
2025-11-14 10:18:20 +0100

    Update config.js

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -34,7 +34,7 @@ var siteConfig = {
       zoom: 1
     },
     // all the columns that are available to the user. This array defines the order they appear in. By default all all column are available.
-    availableTableColumns: ['features', 'country', 'stateProvince', 'locality', 'coordinates', 'year', 'eventDate', 'basisOfRecord', 'recordedBy', 'identifiedBy', 'recordNumber', 'catalogNumber', 'collectionCode', 'institutionCode', 'dataset'],
+    availableTableColumns: ['features', 'country', 'stateProvince', 'locality', 'coordinates', 'year', 'eventDate', 'basisOfRecord', 'recordedBy', 'identifiedBy', 'recordNumber', 'catalogNumber', 'collectionCode', 'institutionCode', 'dataset', 'publishingOrg'],
     defaultTableColumns: ['features', 'country', 'coordinates', 'year', 'catalogNumber', 'dataset'], // the columns showed by default. The order is not relevant, as it is defined in the list of available columns. The user can change what columns to show in the UI.
     // You probably need help to configure the scope - so just ask
     // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
